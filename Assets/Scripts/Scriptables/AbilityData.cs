2058c789f44589b0c76e2c095d08570093f01475
Vitzual <65577955+Vitzual@users.noreply.github.com>
2022-03-10 19:56:25 -0800

    Add chroma data

--- Assets/Scripts/Scriptables/AbilityData.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Card", menuName = "Cards/Powerup")]
-public class AbilityData : CardData
-{
-    
-}
