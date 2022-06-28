52e98807f5e9cd2c3ac3e3025889ce6e97beac44
Vitzual <65577955+Vitzual@users.noreply.github.com>
2022-06-28 14:35:45 -0700

    Add Ironclad arena chip

--- Assets/Scripts/Scriptables/ChipData.cs
+++ Assets/Scripts/Scriptables/ChipData.cs
@@ -2,7 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(fileName = "New Chip", menuName = "Arena/Chip")]
 public class ChipData : IdentifiableScriptableObject
 {
-    
+    public Stat stat;
+    public float effect;
+    public CrystalType crystal;
+    public int cost;
+    public Difficulty difficulty;
 }
