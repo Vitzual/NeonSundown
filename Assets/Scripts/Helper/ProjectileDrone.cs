dde09844e01c366a7215e90318d7496d8a0475a4
Vitzual <65577955+Vitzual@users.noreply.github.com>
2022-11-29 18:13:32 -0700

    Buff projectile drone (duplicate + improve pathing)

--- Assets/Scripts/Helper/ProjectileDrone.cs
+++ Assets/Scripts/Helper/ProjectileDrone.cs
@@ -7,6 +7,7 @@ public class ProjectileDrone : Drone
     // Auto shooter variables
     public AutoShooter projectileOne, projectileTwo;
     public WeaponData weaponOne, weaponTwo;
+    protected ProjectileDrone droneOne, droneTwo;
 
     // Setup the projectile drone
     public void Start()
@@ -15,10 +16,54 @@ public class ProjectileDrone : Drone
         projectileTwo.Setup(weaponTwo);
     }
 
+    // Virtual setup method
+    public override void Setup(Ship ship, HelperData data)
+    {
+        this.ship = ship;
+        this.data = data;
+
+        droneOne = Instantiate(this, new Vector3(transform.position.x + 5f, transform.position.y + 5f, 0f), Quaternion.identity);
+        droneTwo = Instantiate(this, new Vector3(transform.position.x - 5f, transform.position.y - 5f, 0f), Quaternion.identity);
+    }
+
     // Override custom update
     public override void CustomUpdate()
     {
         projectileOne.Use();
         projectileTwo.Use();
+
+        droneOne.CloneUpdate();
+        droneTwo.CloneUpdate();
+    }
+
+    public void CloneUpdate()
+    {
+        projectileOne.Use();
+        projectileTwo.Use();
+    }
+
+    // Moves the drone
+    public override void Move()
+    {
+        if (ship == null) return;
+
+        // Check position relative to player
+        if (Vector2.Distance(transform.position, ship.transform.position) > range)
+        {
+            // Rotate towards the object
+             float angle = Mathf.Atan2(ship.transform.position.y - transform.position.y,
+                ship.transform.position.x - transform.position.x) * Mathf.Rad2Deg;
+            Quaternion targetRotation = Quaternion.Euler(new Vector3(0, 0, angle - 90f));
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 250f * Time.deltaTime);
+        }
+
+        else
+        {
+            // Rotate randomly
+            transform.Rotate(Vector3.forward, 250f * Time.deltaTime);
+        }
+
+        // Move forward
+        transform.position += transform.up * movementSpeed * Time.fixedDeltaTime;
     }
 }
